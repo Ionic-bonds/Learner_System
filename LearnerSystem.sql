026cb137e30a42fab24a217488c018966e0d6a07
Leonardwkwong <leonard_wong@live.com>
2021-10-02 11:44:05 +0800

    Updated learnersystem and created learnersystem sql file

---
+++ LearnerSystem.sql
@@ -0,0 +1,8 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `LearnerSystem`
+--
+CREATE DATABASE IF NOT EXISTS `LearnerSystem` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `LearnerSystem`;
