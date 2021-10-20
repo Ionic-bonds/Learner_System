3beede125b23f3f0c9b1858c6092e8d47d72f5bc
kingsoncjx <72540450+kingsoncjx@users.noreply.github.com>
2021-10-20 21:15:16 +0800

    Update LearnerSystem.js

--- LearnerSystem.js
+++ LearnerSystem.js
@@ -1,7 +1,15 @@
 function retrieveLoad(){
-
+    var apiKey = ''
+    var stripe;
 }
 
+var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var result = JSON.parse(this.responseText);
+        }
+    }
+    
 function retrieveLearnerData(){
     
 }
