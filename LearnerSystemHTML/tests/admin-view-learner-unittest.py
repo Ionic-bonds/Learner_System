960be9f7aacf6e0df346712035b990fcb8121f11
kingsoncjx <72540450+kingsoncjx@users.noreply.github.com>
2021-11-07 21:17:10 +0800

    Create admin-view-learner-unittest.py

---
+++ LearnerSystemHTML/tests/admin-view-learner-unittest.py
@@ -0,0 +1,15 @@
+import unittest
+from PersonClass import Learner
+from PersonClass import Trainer
+from PersonClass import CourseRecord
+from PersonClass import ClassDescription
+
+class TestLearner(unittest.TestCase):
+    def test_learner(self):
+        
+    def test_trainer(self):
+
+    def test_course(self):
+
+    def test_class(self):
+        
