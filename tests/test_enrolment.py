b2a32f04fdbffbdd5e7f4dd66000b77014a699f7
jiaqichai2019 <jiaqi.chai.2019@scis.smu.edu.sg>
2021-11-09 21:07:29 +0800

    fixed tdd error

--- tests/test_enrolment.py
+++ tests/test_enrolment.py
@@ -1,7 +1,4 @@
 # JiaQi's TDD
-
-import os
-from os import path
 import PersonClass
 #PersonClass.path.append('/.../spm-project/')
 from PersonClass import Enrollment
@@ -65,12 +62,31 @@ def test_get_enrolment_byID():
     client = app.test_client()
     url = 'http://3.144.166.168:5016/getEnrollment/1'
 
+    response = client.get(url)
+    assert response.status_code == 200
+
+def test_get_selfEnroll():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://spm:spmteam09@spm-database-1.cujkm1zfxmqs.us-east-2.rds.amazonaws.com:3306/LearnerSystem'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
+    app.config['DEBUG'] = True
+    app.config['TESTING'] = True
+
+    db = SQLAlchemy(app)
+    CORS(app)
+    configure_routes(app)
+    client = app.test_client()
+    url = 'http://3.144.166.168:5016/insertSelfEnrol'
+
+    mock_headers ={'Content-type': 'application/json', 'Accept': 'text/plain'}
     mock_request_data = {
         'LearnerID': 1,
         'CourseID': 1,
+        'ClassID':1,
         'Approved': False,
         'passPrerequisite': False
     }
 
-    response = client.get(url, data= mock_request_data)
+    response = client.post(url,data=json.dumps(mock_request_data), headers = mock_headers)
     assert response.status_code == 200
